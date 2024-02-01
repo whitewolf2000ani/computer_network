474e854a8e373f3657493de9aa05a2ea6ec13ff3
aniketkumar <kaniket123456789@gmail.com>
2024-02-01 23:49:13 +0530

    added modulo2 divison code

--- ntwrol.c
+++ ntwrol.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<string.h>
+void modulo2divison(char divd[100],char key[100],int keyl,int datal);
 
 int main(){
   char key[100],data[100],temp[100],quo[100],rem[100];
@@ -21,24 +22,33 @@ int main(){
     temp[i]=data[i];
 
   }
-  
-  
+  //  padding the duplicate dividend with k-1 "0"
   for(i=0;i<keyl-1;i++){
     temp[datal+i]='0';
   }
-  
-  
-
-
-
-
-
-
-
-
-
-  printf("%s\n",temp);
-
+  modulo2divison(temp,key,keyl,datal);
   return 0;
+}
 
+void modulo2divison(char divd[100],char key[100],int keyl,int datal){
+  int i,j;
+  char rem[100];
+  char chiro[]={0,0,0,0};
+  while(datal>keyl){
+    if(divd[0]==1){
+      for(i=0;i<keyl;i++){
+          rem[i]=divd[i]^key[i];
+      }
+    }
+    else{
+      for(i=0;i<keyl;i++){
+        rem[i]=divd[i]^chiro[i];
+      }
+    }
+    for(i=0;i<keyl;i++){
+      rem[i]<<=1; 
+    }
+    strcpy(divd,rem);
+  }
+  printf("%s",divd);
 }
